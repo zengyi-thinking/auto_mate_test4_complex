69e03fa055f8671d933376d1dac6a22079404209
zengyi-thinking <3355262104@qq.com>
2025-07-25 15:25:28 +0800

    solidity第10课，组合使用

--- Contract.sol
+++ Contract.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity  >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
 
@@ -19,6 +19,11 @@ contract ZombieFactory {
     function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 
 }
