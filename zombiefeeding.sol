cb9b42588a9c190aa1236c46c6f385739f40f68a
zengyi-thinking <3355262104@qq.com>
2025-07-25 16:11:08 +0800

    第19课，僵尸吃啥

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,5 +1,19 @@
 pragma solidity >=0.5.0 <0.6.0;
 import "./zombiefactory.sol";
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
 contract ZombieFeeding is ZombieFactory {
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
