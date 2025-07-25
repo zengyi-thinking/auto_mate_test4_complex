6195401d13055e41b0618210938fb2d44f87227d
zengyi-thinking <3355262104@qq.com>
2025-07-25 15:10:07 +0800

    solidity第七课，学习结构体和数组使用

--- Contract.sol
+++ Contract.sol
@@ -12,8 +12,8 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function createZombie(string memory _name, uint _dna) public {
-
+    function createZombie (string memory _name, uint _dna) public {
+        zombies.push(Zombie(_name, _dna));
     }
 
 }
